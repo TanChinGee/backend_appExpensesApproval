c89b87200a35c3f7395c4fee188b014df727630c
JCG Tan <jackytancg@gmail.com>
2020-08-21 01:33:04 +0800

    added test file

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,17 @@
-const request = require('supertest');
-const app = require('../server');
+const app = require("../server");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
-describe('GET /', function() {
-  it('return json response', function() {
-    return request(app)
-      .get('/')
-      .expect(200)
-      .expect('Content-Type',/json/)
-      .expect('{"text":"some json"}')
-  })
+const { expect } = chai;
+chai.use(chaiHttp);
+describe("Server!", () => {
+  it("Welcome to JT application.", done => {
+    chai
+      .request(app)
+      .get("/")
+      .end((err, res) => {
+        expect(res.body.message).to.equals("Welcome to JT application.");
+        done();
+      });
+  });
 })
