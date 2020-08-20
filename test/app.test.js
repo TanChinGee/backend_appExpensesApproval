e7013e31044f6149d06410ef0a8485bdc5d66bd4
JCG Tan <jackytancg@gmail.com>
2020-08-21 00:14:40 +0800

    added test file

---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+const app = require("../server");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const { expect } = chai;
+chai.use(chaiHttp);
+describe("Server!", () => {
+  it("Welcome to JT application.", done => {
+    chai
+      .request(app)
+      .get("/")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals("Welcome to JT application.");
+        done();
+      });
+  });
+})
